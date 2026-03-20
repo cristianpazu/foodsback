3aa69ba861d8ca44e5f753b11eb96842e2218394
cr <cbalanta@playtechla.com>
2026-03-20 16:26:34 -0500

    cambio

--- foods/src/main/java/com/example/foods/repository/pedidos/PedidoRepository.java
+++ foods/src/main/java/com/example/foods/repository/pedidos/PedidoRepository.java
@@ -17,4 +17,31 @@ Optional<HistorialPedidoDTO> findByPedidos();
 select p.id_pedido, m.nombre,p2.nombre, p2.precio, ped.cantidad, p.fecha, p.total_cuenta
 from pedido p inner join mesas m on m.id_mesa = p.mesa_id
     inner join pedido_item ped on p.id_pedido = ped.pedido_id inner join public.productos p2 on p2.id_productos = ped.productos_id;*/
+    /*
+    http://localhost:8080/Pedido/pedido
+    {
+  "mesas": {
+    "idMesa": 1
+  },
+  "items": [
+    {
+      "productos": {
+        "idProductos": 6
+      },
+      "cantidad": 3
+    },
+    {
+      "productos": {
+        "idProductos": 11
+      },
+      "cantidad": 3
+    }
+  ]
+}http://localhost:8080/restaurante/traer/menuTodo/1http://localhost:8080/submenu/traer/submenus
+http://localhost:8080/Mesas/mesa
+{
+    "nombre": "Mesa 1",
+    "disponibildad": true
+}
+     */
 }
